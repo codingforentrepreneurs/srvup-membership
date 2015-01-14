522e6c7d71086179e1044af3f56ec5e835083c8d
Coding for Entrepreneurs <codingforentrepreneurs@gmail.com>
2015-01-14 13:41:54 -0800

    8 - Embed from YouTube

--- src/srvup/views.py
+++ src/srvup/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
-
+from django.utils.safestring import mark_safe
 from videos.models import Video
 
 def home(request):
 	name = "Justin"
 	videos = Video.objects.all()
 	embeds = []
+
 	for vid in videos:
-		embeds.append("%s" %(vid.embed_code))
+		code = mark_safe(vid.embed_code)
+		embeds.append("%s" %(code))
 
 	context = {
 		"the_name": name,
 		"number": videos.count(),
 		"videos": videos,
 		"the_embeds": embeds,
+		"a_code": mark_safe(videos[0].embed_code)
 	}
 	return render(request, "home.html", context)
 	#return render_to_response("home.html", context, context_instance=RequestContext(request))
