33037009278967315ab010f82569899ecf23c708
Coding for Entrepreneurs <codingforentrepreneurs@gmail.com>
2015-02-04 16:22:56 -0800

    67 - Charge Subscription

--- src/billing/views.py
+++ src/billing/views.py
@@ -27,13 +27,13 @@ def upgrade(request):
 	if request.user.is_authenticated():
 		try:
 			#something to get the current customer id stored somewhere 
-			merchant_customer_id = UserMerchantId.objects.get(user=request.user).customer_id
+			merchant_obj = UserMerchantId.objects.get(user=request.user)
 		except UserMerchantId.DoesNotExist:
 			new_customer_result = braintree.Customer.create({})
 			if new_customer_result.is_success:
-				merchant_customer_id = UserMerchantId.objects.create(user=request.user)
-				merchant_customer_id.customer_id = new_customer_result.customer.id
-				merchant_customer_id.save()
+				merchant_obj, created = UserMerchantId.objects.get_or_create(user=request.user)
+				merchant_obj.customer_id = new_customer_result.customer.id
+				merchant_obj.save()
 				print """Customer created with id = {0}""".format(new_customer_result.customer.id)
 			else:
 				print "Error: {0}".format(new_customer_result.message)
@@ -43,14 +43,32 @@ def upgrade(request):
 			#redirect somewhere
 			pass
 
-		trans = Transaction.objects.create_new(request.user,\
-				 "aslkdfjasdf%s"%(random.randint(0,100)),\
-				  25.00, "visa")
-		if trans.success:
-			membership_instance, created = Membership.objects.get_or_create(user=request.user)
-			membership_dates_update.send(membership_instance, new_date_start=trans.timestamp)
+		merchant_customer_id = merchant_obj.customer_id
+		if request.method == "POST":
+			nonce = request.POST.get("payment_method_nonce", None)
+			if nonce is not None:
+				customer_update_result = braintree.Customer.update(merchant_customer_id, {
+					"payment_method_nonce": nonce
+					})
+				credit_card_token = customer_update_result.customer.credit_cards[0].token
+				subscription_result = braintree.Subscription.create({
+						"payment_method_token": credit_card_token,
+						"plan_id": PLAN_ID
+					})
+				if subscription_result.is_success:
+					trans_id = subscription_result.subscription.id
+					trans = Transaction.objects.create_new(request.user,trans_id,25.00, "visa")
+					if trans.success:
+						membership_instance, created = Membership.objects.get_or_create(user=request.user)
+						membership_dates_update.send(membership_instance, new_date_start=trans.timestamp)
+				else:
+					print "failed"
+					
+				# customer_card = braintree.Customer.find(merchant_customer_id).credit_cards[0].token
+
+			
 	context =  {"client_token":client_token}
-	
+
 	return render(request, "billing/upgrade.html",context)
 
 
